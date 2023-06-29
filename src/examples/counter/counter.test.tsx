7551f36261173fcf9f8718e95177bc7fe6ffa52b
Rosen Toshev <rtoscheff@gmail.com>
2023-06-29 15:54:39 +0100

    Created a special version of render that sets up the user and returns the user

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,18 +1,19 @@
 // @vitest-environment happy-dom
 
-import { screen, render, fireEvent } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
+import { render } from './test/utilities';
 
 test('it should render the component', () => {
   render(<Counter />);
 });
 
-test('it should increment when the "Increment" button is pressed', () => {
-  render(<Counter />);
+test('it should increment when the "Increment" button is pressed', async () => {
+  const { user } = render(<Counter />);
   const currentCount = screen.getByTestId('current-count');
   expect(currentCount).toHaveTextContent('0');
   const incrementButton = screen.getByRole('button', { name: 'Increment' });
-  fireEvent.click(incrementButton);
+  await user.click(incrementButton);
   expect(currentCount).toHaveTextContent('1');
 });
