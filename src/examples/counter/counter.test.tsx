a604180c299ac312b419a4750782eeb23c2f623f
Rosen Toshev <rtoscheff@gmail.com>
2023-06-29 15:21:46 +0100

    Added an onClick firevent when the user clicks on the increment button

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,18 @@
-import { screen, render } from '@testing-library/react';
+// @vitest-environment happy-dom
+
+import { screen, render, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+});
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {},
-);
+test('it should increment when the "Increment" button is pressed', () => {
+  render(<Counter />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('0');
+  const incrementButton = screen.getByRole('button', { name: 'Increment' });
+  fireEvent.click(incrementButton);
+  expect(currentCount).toHaveTextContent('1');
+});
