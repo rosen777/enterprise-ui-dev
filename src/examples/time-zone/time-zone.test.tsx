06f4019ca29e135728ddc2cfa5c5e1fbf4cd6830
Rosen Toshev <rtoscheff@gmail.com>
2023-06-30 18:27:32 +0100

    Added mocks, spies, and fake timers

--- src/examples/time-zone/time-zone.test.tsx
+++ src/examples/time-zone/time-zone.test.tsx
@@ -1,12 +1,23 @@
-import { test, expect, vi } from 'vitest';
+import { test, expect, vi, beforeEach, afterEach } from 'vitest';
 import { render } from 'test/utilities';
 import TimeZone from '.';
+
+const date = new Date(2023, 1, 1);
+
+beforeEach(() => {
+  vi.useFakeTimers();
+  vi.setSystemTime(date);
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
 
 test('it should render successfully', () => {
   render(<TimeZone />);
 });
 
-test.fails('should match the snapshot', async () => {
+test('should match the snapshot', async () => {
   const { container } = render(<TimeZone />);
   expect(container).toMatchSnapshot();
 });
